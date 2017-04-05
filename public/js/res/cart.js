0a1892735a377da3477c5f5f21d2d44cf253dcc9
Philic <xlw125414713@163.com>
2017-04-05 15:47:05 +0800

    cart page update

--- public/js/res/cart.js
+++ public/js/res/cart.js
@@ -8,10 +8,23 @@ app.controller('CartCtrl', ['$scope', '$http', '$localStorage', function($scope,
     console.log('haha');
     console.log($localStorage.cart);
     $scope.bookCart = $localStorage.cart;
-    
+
+    // style config -------------------------
+    var countBtnWidth = Math.round(window.innerWidth*0.1*1.1) + 'px';
+    console.log(countBtnWidth);
     $scope.countBtnStyle = {
-        
-    }
+        "width":              countBtnWidth,
+        "border":             "1px solid",
+        "border-color":       "red",
+        "background-color":   "#FFFFFF"
+    };
+    var submitBtnMargin = Math.round(window.innerWidth*0.1*0.1) + 'px';
+    var submitBtnWidth  = Math.round(window.innerWidth*0.1*0.8) + 'px';
+    $scope.submitBtnStyle = {
+        "width": submitBtnWidth,
+        "margin-left": submitBtnMargin
+    };
+    //!style config -------------------------
     $scope.rmBook = function(index) {
         
     }
